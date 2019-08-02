3308e4c67fc5efe71c4a943fd19c5d6acbfa29ed
DMW007 <danielw@posteo.de>
2019-08-02 20:36:21 +0200

    Removed some example data from the project template

--- ULabs.VBulletinEntityDemo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace ULabs.VBulletinEntityDemo.Models {
-    public class ErrorViewModel {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
