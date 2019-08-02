a63942b5e6123a21a0e3d1dc72542f46d488e1ef
DMW007 <danielw@posteo.de>
2019-08-02 21:32:10 +0200

    Refactoring VBUserGroup

--- ULabs.VBulletinEntity/Models/User/VBUserGroup.cs
+++ ULabs.VBulletinEntity/Models/User/VBUserGroup.cs
@@ -11,22 +11,19 @@ namespace ULabs.VBulletinEntity.Models.User {
         [Column("usergroupid")]
         public int Id { get; set; }
 
-        [Column("title"), MaxLength(100)]
+        [MaxLength(100)]
         public string Title { get; set; }
 
-        [Column("description"), MaxLength(500)]
+        [MaxLength(250)]
         public string Description { get; set; }
 
-        [Column("usertitle"), MaxLength(100)]
+        [MaxLength(100)]
         public string UserTitle { get; set; }
 
         [Column("passwordexpires")]
         public int PasswordExpiresDays { get; set; }
 
-        [Column("passwordhistory")]
         public int PasswordHistory { get; set; }
-
-        [Column("pmquota")]
         public int PmQuota { get; set; }
 
         [Column("pmsendmax")]
@@ -38,33 +35,24 @@ namespace ULabs.VBulletinEntity.Models.User {
         [Column("closetag"), MaxLength(100)]
         public string HtmlCloseTag { get; set; }
 
-        [Column("canoverride", TypeName = "SMALLINT")]
+        [Column(TypeName = "SMALLINT")]
         public bool CanOverride { get; set; }
 
-        [Column("ispublicgroup", TypeName = "SMALLINT")]
+        [Column(TypeName = "SMALLINT")]
         public bool IsPublicGroup { get; set; }
 
         // ToDo: Permissions (see Overview-Project for implementation that can be re-used)
-        [Column("forumpermissions")]
+        // Missing: pmpermissions, calendarpermissions, wolpermissions, genericpermissions, genericpermissions2, genericoptions, signaturepermissions, visitormessagepermissions
         public VBForumFlags ForumPermissions { get; set; }
 
         // First test case for directly using flags without custom int attribute for mapping
-        // ToDo: Merge ForumPermissionsRaw with ForumPermissions if it works 
-        [Column("adminpermissions")]
         public VBAdminFlags AdminPermissions { get; set; }
 
         [Column("attachlimit")]
         public int AttachmentLimit { get; set; }
-
-        [Column("avatarmaxwidth")]
         public int AvatarMaxWidth { get; set; }
-
-        [Column("avatarmaxheight")]
         public int AvatarMaxHeight { get; set; }
-
-        [Column("avatarmaxsize")]
         public int AvatarMaxSize { get; set; }
-
-        // ToDo: Profile pic and signature 
+        public int ProfilePicMaxWidth { get; set; }
     }
 }
