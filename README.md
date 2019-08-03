98af3123311211457f25c97cb56abfc334a413ab
DMW007 <danielw@posteo.de>
2019-08-03 14:15:09 +0200

    Begin with adding missing VBUser attributes

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ It is based on the current V2.1 LTS release of Entity Framework Core.
 
 | Entity  | State | Comment |
 | ------------- | ------------- |
-| VBUserGroup  | ![Partly implemented](https://u-img.net/img/5113Ab.png) | Forum & Admin permissions implemented, rest missing
+| VBUserGroup  | ![Partly implemented](https://u-img.net/img/5113Ab.png) | Forum & Admin permissions implemented, rest of the permissions missing
+| VBUser | x | x
 
 ![Complete](https://u-img.net/img/8798Dw.png) Full completed 
 ![Partly implemented](https://u-img.net/img/5113Ab.png) Partly implemented
--- ULabs.VBulletinEntity/Models/User/VBUser.cs
+++ ULabs.VBulletinEntity/Models/User/VBUser.cs
@@ -1,8 +1,10 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Net;
 using System.Text;
 using ULabs.VBulletinEntity.Models.Forum;
 using ULabs.VBulletinEntity.Tools;
@@ -13,13 +15,106 @@ namespace ULabs.VBulletinEntity.Models.User {
     public class VBUser {
         [Column("userid")]
         public int Id { get; set; }
-
         public int UserGroupId { get; set; }
         public VBUserGroup UserGroup { get; set; }
 
         [Column("membergroupids")]
         public string MemberGroupIdsRaw { get; set; }
+        public int? DisplayGroupId { get; set; }
+        public VBUserGroup DisplayGroup { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
 
+        [Column(TypeName = "DATE")]
+        public DateTime PasswordDate { get; set; }
+        public string Email { get; set; }
+        public int StyleId { get; set; }
+
+        [MaxLength(50)]
+        public string ParentEmail { get; set; }
+
+        [MaxLength(100)]
+        public string Homepage { get; set; }
+
+        [MaxLength(20)]
+        public string Icq { get; set; }
+        [MaxLength(20)]
+        public string Aim { get; set; }
+        [MaxLength(20)]
+        public string Yahoo { get; set; }
+        [MaxLength(20)]
+        public string MSN { get; set; }
+        [MaxLength(20)]
+        public string Skype { get; set; }
+
+        // ToDo: Generate enum (Value range seems to be from 0 to 2)
+        public int ShowVBCode { get; set; }
+        public int ShowBirthday { get; set; }
+
+        [MaxLength(250)]
+        public string UserTitle { get; set; }
+        // ToDo: Generate enum
+        public int CustomTitle { get; set; }
+
+        [Column("joindate")]
+        public int JoinDateRaw { get; set; }
+
+        // ToDo: Generate enum
+        public int Daysprune { get; set; }
+
+        [Column("lastvisit")]
+        public int LastVisitTimeRaw { get; set; }
+
+        [Column("lastactivity")]
+        public int LastActivityTimeRaw { get; set; }
+
+        [Column("lastpost")]
+        public int LastPostTimeRaw { get; set; }
+        public int LastPostId { get; set; }
+        [JsonProperty(ReferenceLoopHandling = ReferenceLoopHandling.Ignore)]
+        public VBPost LastPost { get; set; }
+
+        [Column("posts")]
+        public int PostsCount { get; set; }
+        public int Reputation { get; set; }
+        public int ReputationLevelId { get; set; }
+        [Column("timezoneoffset"), MaxLength(4)]
+        public string TimezoneOffsetRaw { get; set; }
+        // ToDo: Create enum
+        public int PmPopup { get; set; }
+
+        public int AvatarId { get; set; }
+        public int AvatarRevision { get; set; }
+        public int ProfilePicRevision { get; set; }
+        public int SigPicRevision { get; set; }
+        // ToDo: Create enum
+        public int Options { get; set; }
+
+        // birthday_search not implemented here since it contains the same data than Birthday but in another format
+        [Column("birthday"), DataType(DataType.Date)]
+        public string BirthdayRaw { get; set; }
+        // Seems like the post per page settings - ToDo: Verify and document
+        public int MaxPosts { get; set; }
+
+        public int StartOfWeek { get; set; }
+
+        [Column(TypeName = "VARCHAR(45)")]
+        public string IPAddress { get; set; }
+        public VBCustomAvatar CustomAvatar { get; set; }
+
+        public int ReferrerId { get; set; }
+        public VBUser Referrer { get; set; }
+        // ToDo: Check FK
+        public int LanguageId { get; set; }
+
+        [Column("emailstamp")]
+        public int EmailStampRaw { get; set; }
+        // ToDo: Handle self referencing loops doesn't full work yet. On the session we get: 
+        // JsonSerializationException: Self referencing loop detected with type 'ULabs.VBulletinEntity.Models.Forum.VBPost'. Path 'User.LastPost.Thread.Replys'.
+        [JsonProperty(ReferenceLoopHandling = ReferenceLoopHandling.Ignore)]
+        public List<VBPost> Posts { get; set; }
+
+        #region NotMapped 
         [NotMapped]
         public List<int> MemberGroupIds {
             get {
@@ -35,33 +130,30 @@ namespace ULabs.VBulletinEntity.Models.User {
                 MemberGroupIdsRaw = string.Join(",", value);
             }
         }
-
-        public int? DisplayGroupId { get; set; }
-        public VBUserGroup DisplayGroup { get; set; }
-
-        public string UserName { get; set; }
-
-        public string Password { get; set; }
-
-        public int LastPostId { get; set; }
-        [JsonProperty(ReferenceLoopHandling = ReferenceLoopHandling.Ignore)]
-        public VBPost LastPost { get; set; }
-
-        [Column("posts")]
-        public int PostsCount { get; set; }
-
-        [Column("joindate")]
-        public int JoinDateRaw { get; set; }
+        [NotMapped]
+        public DateTime Birthday {
+            get => DateTime.Parse(BirthdayRaw);
+            set {
+                BirthdayRaw = value.ToString("MM-dd-yy");
+            }
+        }
+        [NotMapped]
+        public int TimezoneOffset {
+            get => int.Parse(TimezoneOffsetRaw);
+            set => TimezoneOffsetRaw = value.ToString();
+        }
 
         [NotMapped]
         public DateTime JoinDate {
-            get { return DateTimeExtensions.ToDateTime(JoinDateRaw); }
-            set { JoinDateRaw = DateTimeExtensions.ToUnixTimestampAsInt(value); }
+            get => DateTimeExtensions.ToDateTime(JoinDateRaw);
+            set => JoinDateRaw = DateTimeExtensions.ToUnixTimestampAsInt(value);
         }
 
-        public int AvatarRevision { get; set; }
-
-        public VBCustomAvatar CustomAvatar { get; set; }
+        [NotMapped]
+        public DateTime EmailStamp {
+            get => DateTimeExtensions.ToDateTime(EmailStampRaw);
+            set => EmailStampRaw = DateTimeExtensions.ToUnixTimestampAsInt(value);
+        }
 
         [NotMapped]
         public string AvatarFileName {
@@ -72,25 +164,11 @@ namespace ULabs.VBulletinEntity.Models.User {
             }
         }
 
-        public string GetAvatarUrl(string forumBaseUrl) {
-            if (CustomAvatar == null) {
-                // ToDo: VB has not setting for the default Avatar. We should specify this in custom settings somewhere
-                return "https://u-img.net/img/4037Ld.png";
-            }
-            return $"{forumBaseUrl}/customavatars/{AvatarFileName}";
-        }
-
-        [Column("lastvisit")]
-        public int LastVisitTimeRaw { get; set; }
-
         [NotMapped]
         public DateTime LastVisitTime {
             get { return DateTimeExtensions.ToDateTime(LastVisitTimeRaw); }
             set { LastVisitTimeRaw = DateTimeExtensions.ToUnixTimestampAsInt(value); }
         }
-
-        [Column("lastactivity")]
-        public int LastActivityTimeRaw { get; set; }
 
         [NotMapped]
         public DateTime LastActivityTime {
@@ -98,18 +176,19 @@ namespace ULabs.VBulletinEntity.Models.User {
             set { LastActivityTimeRaw = DateTimeExtensions.ToUnixTimestampAsInt(value); }
         }
 
-        [Column("lastpost")]
-        public int LastPostTimeRaw { get; set; }
-
         [NotMapped]
         public DateTime LastPostTime {
             get { return DateTimeExtensions.ToDateTime(LastPostTimeRaw); }
             set { LastPostTimeRaw = DateTimeExtensions.ToUnixTimestampAsInt(value); }
         }
-        // ToDo: Handle self referencing loops doesn't full work yet. On the session we get: 
-        // JsonSerializationException: Self referencing loop detected with type 'ULabs.VBulletinEntity.Models.Forum.VBPost'. Path 'User.LastPost.Thread.Replys'.
-        [JsonProperty(ReferenceLoopHandling = ReferenceLoopHandling.Ignore)]
-        public List<VBPost> Posts { get; set; }
+        public string GetAvatarUrl(string forumBaseUrl) {
+            if (CustomAvatar == null) {
+                // ToDo: VB has not setting for the default Avatar. We should specify this in custom settings somewhere
+                return "https://u-img.net/img/4037Ld.png";
+            }
+            return $"{forumBaseUrl}/customavatars/{AvatarFileName}";
+        }
+        #endregion
     }
 }
 
