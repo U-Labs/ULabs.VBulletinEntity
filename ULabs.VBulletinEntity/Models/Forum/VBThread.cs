506147eb387f4a838053c8609d642db5fc147d1f
DMW007 <danielw@posteo.de>
2019-08-04 19:07:16 +0200

    Added missing VBThread fields and modified existing ones for coding conventions

--- ULabs.VBulletinEntity/Models/Forum/VBThread.cs
+++ ULabs.VBulletinEntity/Models/Forum/VBThread.cs
@@ -8,6 +8,7 @@ using System.Text.RegularExpressions;
 using ULabs.VBulletinEntity.Models.User;
 using ULabs.VBulletinEntity.Tools;
 using System.Web;
+using System.Linq;
 
 namespace ULabs.VBulletinEntity.Models.Forum {
     [Table("thread")]
@@ -16,42 +17,22 @@ namespace ULabs.VBulletinEntity.Models.Forum {
         [Column("threadid")]
         public int Id { get; set; }
 
-        [Column("title"), MaxLength(250)]
+        [MaxLength(250)]
         public string Title { get; set; }
 
-        [NotMapped]
-        public string HtmlDecodedTitle {
-            get {
-                return HttpUtility.HtmlDecode(Title);
-            }
-        }
-
-        [NotMapped]
-        public string SeoTitle {
-            get => ContentTools.SeoTitle(Title);
-        }
-
-        [Column("prefixid"), MaxLength(25)]
+        [MaxLength(25)]
         public string PrefixId { get; set; }
 
-        [Column("firstpostid")]
         public int FirstPostId { get; set; }
         [JsonProperty(ReferenceLoopHandling = ReferenceLoopHandling.Ignore)]
         public VBPost FirstPost { get; set; }
 
-        [Column("lastpostid")]
         public int LastPostId { get; set; }
         [JsonProperty(ReferenceLoopHandling = ReferenceLoopHandling.Ignore)]
         public VBPost LastPost { get; set; }
 
         [Column("lastpost")]
         public int LastPostTimeRaw { get; set; }
-
-        [NotMapped]
-        public DateTime LastPostTime {
-            get { return LastPostTimeRaw.ToDateTime(); }
-            set { LastPostTimeRaw = DateTimeExtensions.ToUnixTimestampAsInt(value); }
-        }
 
         public int ForumId { get; set; }
         public VBForum Forum { get; set; }
@@ -64,7 +45,7 @@ namespace ULabs.VBulletinEntity.Models.Forum {
         public bool IsOpen { get; set; }
 
         [Column("replycount")]
-        public int ReplyPostsCount { get; set; }
+        public int ReplysCount { get; set; }
 
         [Column("hiddencount")]
         public int HiddenPostsCount { get; set; }
@@ -86,17 +67,11 @@ namespace ULabs.VBulletinEntity.Models.Forum {
         [Column("dateline")]
         public int CreatedTimeRaw { get; set; }
 
-        [NotMapped]
-        public DateTime CreatedTime {
-            get { return CreatedTimeRaw.ToDateTime(); }
-            set { CreatedTimeRaw = DateTimeExtensions.ToUnixTimestampAsInt(value); }
-        }
-
         public int Views { get; set; }
 
         public int IconId { get; set; }
 
-        [Column("notes"), MaxLength(250)]
+        [MaxLength(250)]
         public string Notes { get; set; }
 
         [Column("visible")]
@@ -111,16 +86,20 @@ namespace ULabs.VBulletinEntity.Models.Forum {
         [Column("votetotal")]
         public int VotesScore { get; set; }
 
-        // ToDo: attach, similar, taglist
+        [Column("attach")]
+        public int AttachmentsCount { get; set; }
+
+        [Column("similar"), MaxLength(55)]
+        public string SimilarThreadIdsRaw { get; set; }
+        public string Taglist { get; set; }
 
         [Column("lastposterid")]
         public int LastPostAuthorId { get; set; }
         [JsonProperty(ReferenceLoopHandling = ReferenceLoopHandling.Ignore)]
         public VBUser LastPostAuthor { get; set; }
 
-        /// <summary>
-        /// Anzahl der Nutzer, die im Thema einen Beitrag geschrieben haben
-        /// </summary>
+        [Column("keywords")]
+        public string KeywordsRaw { get; set; }
         public int PosterCount { get; set; }
 
         [JsonProperty(ReferenceLoopHandling = ReferenceLoopHandling.Ignore)]
@@ -128,8 +107,8 @@ namespace ULabs.VBulletinEntity.Models.Forum {
 
         public VBThread() { }
 
-        public VBThread(int lastPostAuthorId, string lastPostAuthorName, int firstPostId, int lastPostId, int forumId, int authorId, string authorName, DateTime createdTime, DateTime lastPostTime, 
-            string title, bool isOpen = true, bool isVisible = true, int views = 0, string notes = "", string prefixId="") {
+        public VBThread(int lastPostAuthorId, string lastPostAuthorName, int firstPostId, int lastPostId, int forumId, int authorId, string authorName, DateTime createdTime, DateTime lastPostTime,
+            string title, bool isOpen = true, bool isVisible = true, int views = 0, string notes = "", string prefixId = "") {
             LastPostAuthorId = lastPostAuthorId;
             LastPostAuthorName = lastPostAuthorName;
             FirstPostId = firstPostId;
@@ -145,6 +124,44 @@ namespace ULabs.VBulletinEntity.Models.Forum {
             Views = views;
             Notes = notes;
             PrefixId = prefixId;
+        }
+
+        [NotMapped]
+        public List<int> SimilarThreadIds {
+            get => SimilarThreadIdsRaw.Split(',')
+                .Select(int.Parse)
+                .ToList();
+            set => SimilarThreadIdsRaw = string.Join(",", value);
+        }
+
+        [NotMapped]
+        public List<string> Keywords {
+            get => KeywordsRaw.Split(',').ToList();
+            set => KeywordsRaw = string.Join(",", value);
+        }
+
+        [NotMapped]
+        public string HtmlDecodedTitle {
+            get {
+                return HttpUtility.HtmlDecode(Title);
+            }
+        }
+
+        [NotMapped]
+        public string SeoTitle {
+            get => ContentTools.SeoTitle(Title);
+        }
+
+        [NotMapped]
+        public DateTime LastPostTime {
+            get { return LastPostTimeRaw.ToDateTime(); }
+            set { LastPostTimeRaw = DateTimeExtensions.ToUnixTimestampAsInt(value); }
+        }
+
+        [NotMapped]
+        public DateTime CreatedTime {
+            get { return CreatedTimeRaw.ToDateTime(); }
+            set { CreatedTimeRaw = DateTimeExtensions.ToUnixTimestampAsInt(value); }
         }
     }
 }
