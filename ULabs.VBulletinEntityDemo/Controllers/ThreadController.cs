4fc02ab865610dd99a8ac87e3eaad160eecdb53c
DMW007 <danielw@posteo.de>
2019-08-04 22:47:04 +0200

    View-Thread simple example added

---
+++ ULabs.VBulletinEntityDemo/Controllers/ThreadController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ULabs.VBulletinEntity.Manager;
+using ULabs.VBulletinEntity.Models.Forum;
+
+namespace ULabs.VBulletinEntityDemo.Controllers {
+    public class ThreadController : Controller {
+        readonly VBThreadManager threadManager;
+        public ThreadController(VBThreadManager threadManager) {
+            this.threadManager = threadManager;
+        }
+        public async Task<IActionResult> View(int id) {
+            var thread = await threadManager.GetThreadAsync(id);
+            var replys = await threadManager.GetReplysAsync(id);
+            var model = new KeyValuePair<VBThread, List<VBPost>>(thread, replys);
+            return View(model);
+        }
+    }
+}
