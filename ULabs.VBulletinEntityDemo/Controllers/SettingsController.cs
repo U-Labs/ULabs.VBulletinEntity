d70b96bbe4a142558a0396cafd60ab7a647bcf91
DMW007 <danielw@posteo.de>
2019-08-05 17:38:45 +0200

    Examples for SettingsManager added

---
+++ ULabs.VBulletinEntityDemo/Controllers/SettingsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ULabs.VBulletinEntity.Manager;
+
+namespace ULabs.VBulletinEntityDemo.Controllers {
+    public class SettingsController : Controller {
+        readonly VBSettingsManager settingsManager;
+        public SettingsController(VBSettingsManager settingsManager) {
+            this.settingsManager = settingsManager;
+        }
+        public IActionResult Index() {
+            var settings = settingsManager.GetCommonSettings();
+            return View(settings);
+        }
+    }
+}
