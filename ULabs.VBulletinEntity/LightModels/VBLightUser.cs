7a03797a9ddd7a4b437a5342111d90cf1f0e2204
DMW007 <danielw@posteo.de>
2019-08-17 22:01:18 +0200

    Added username and title in light user entity

--- ULabs.VBulletinEntity/LightModels/VBLightUser.cs
+++ ULabs.VBulletinEntity/LightModels/VBLightUser.cs
@@ -5,6 +5,8 @@ using System.Text;
 namespace ULabs.VBulletinEntity.LightModels {
     public class VBLightUser {
         public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserTitle { get; set; }
         public int PrimaryUserGroupId { get; set; }
         public int AvatarRevision { get; set; }
     }
