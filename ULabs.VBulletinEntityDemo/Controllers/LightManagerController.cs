194fc06904527ede5f50f58c24601d1dd7e128d3
DMW007 <danielw@posteo.de>
2019-08-17 13:51:22 +0200

    New controller for light manager examples

---
+++ ULabs.VBulletinEntityDemo/Controllers/LightManagerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ULabs.VBulletinEntity.LightManager;
+using ULabs.VBulletinEntity.Manager;
+
+namespace ULabs.VBulletinEntityDemo.Controllers {
+    public class LightManagerController : Controller {
+        readonly VBLightDashboardManager lightDashboardManager;
+        public LightManagerController(VBLightDashboardManager lightDashboardManager) {
+            this.lightDashboardManager = lightDashboardManager;
+        }
+        public async Task<IActionResult> Dashboard() {
+            var newestThreads = lightDashboardManager.GetNewestThreads(10);
+            return View(newestThreads);
+        }
+    }
+}
--- ULabs.VBulletinEntityDemo/Controllers/UserController.cs
+++ ULabs.VBulletinEntityDemo/Controllers/UserController.cs
@@ -10,11 +10,9 @@ namespace ULabs.VBulletinEntityDemo.Controllers {
     public class UserController : Controller {
         readonly VBUserManager userManager;
         readonly VBSessionManager sessionManager;
-        readonly VBLightDashboardManager lightDashboardManager;
-        public UserController(VBUserManager userManager, VBSessionManager sessionManager, VBLightDashboardManager lightDashboardManager) {
+        public UserController(VBUserManager userManager, VBSessionManager sessionManager) {
             this.userManager = userManager;
             this.sessionManager = sessionManager;
-            this.lightDashboardManager = lightDashboardManager;
         }
         public async Task<IActionResult> Profile(int id) {
             var user = await userManager.GetUserAsync(id);
@@ -22,8 +20,6 @@ namespace ULabs.VBulletinEntityDemo.Controllers {
         }
         public async Task<IActionResult> Session() {
             var session = await sessionManager.GetCurrentAsync();
-            var notViewableForumIds = lightDashboardManager.GetForumIdsWithoutViewPermission(session.User.UserGroupId);
-            var categoriesWithChilds = lightDashboardManager.GetCategoriesWithChilds();
             return View(session);
         }
     }
