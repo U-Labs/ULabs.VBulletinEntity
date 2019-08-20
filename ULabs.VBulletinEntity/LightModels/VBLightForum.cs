cabef74fbff22f166e63bdf727226baba2086ec1
DMW007 <danielw@posteo.de>
2019-08-20 12:25:14 +0200

    Fixed typing for Forum Parent Id

--- ULabs.VBulletinEntity/LightModels/VBLightForum.cs
+++ ULabs.VBulletinEntity/LightModels/VBLightForum.cs
@@ -7,7 +7,7 @@ namespace ULabs.VBulletinEntity.LightModels {
     public class VBLightForum {
         public int ForumId { get; set; }
         public string Title { get; set; }
-        public string ParentId { get; set; }
+        public int ParentId { get; set; }
         public string ParentIdsRaw { get; set; }
         public List<int> ParentIds {
             get => ParentIdsRaw?.Split(',')
