76e7b35db4dca91a34fed69ab9f93adbf02456f1
DMW007 <danielw@posteo.de>
2019-08-28 19:41:19 +0200

    Added fetching of private messates and NuGet 0.6 release

--- ULabs.VBulletinEntity/LightManager/VBLightUserManager.cs
+++ ULabs.VBulletinEntity/LightManager/VBLightUserManager.cs
@@ -24,7 +24,6 @@ namespace ULabs.VBulletinEntity.LightManager {
         public VBLightUserManager(MySqlConnection db) {
             this.db = db;
         }
-
         ~VBLightUserManager() {
             db.Close();
         }
@@ -42,6 +41,38 @@ namespace ULabs.VBulletinEntity.LightManager {
             var user = db.Query(sql, mappingFunc, new { userId });
             return user.FirstOrDefault();
         }
+        /// <summary>
+        /// Loads private messages that were send to <paramref name="userId"/>
+        /// </summary>
+        /// <param name="readState">Filter the messages to read/unread or answered messages</param>
+        /// <param name="count">Maximum amount of messages that would be returned</param>
+        /// <param name="textPreviewWords">If set, the messages Text property will be an excerpt to this amound of workds. Setting to null gives the full content</param>
+        /// <returns></returns>
+        public List<VBLightPrivateMessage> GetPrivateMessages(int userId, VBPrivateMessageReadState? readState = null, int count = 10, int? textPreviewWords = null) {
+            Func<VBLightPrivateMessage, VBLightUser, VBLightUserGroup, VBLightPrivateMessage> mappingFunc = (pm, user, group) => {
+                pm.FromUser = user;
+                pm.FromUser.PrimaryUserGroup = group;
+                return pm;
+            };
+            string textSelectColumn = (textPreviewWords.HasValue ? "SUBSTRING_INDEX(txt.message, ' ', @textPreviewWords)" : "txt.message");
+            string sql = $@"
+                SELECT pm.pmid, pm.pmtextid, pm.parentpmid, pm.messageread AS MessageReadRaw, 
+                {textSelectColumn} AS text, txt.title, txt.dateline AS SendTimeRaw, txt.fromuserid AS Id,
+                u.username AS UserName, u.usertitle AS UserTitle, u.lastactivity AS LastActivityRaw, u.avatarrevision AS AvatarRevision, 
+                            g.usergroupid as Id, g.opentag as OpenTag, g.closetag as CloseTag, g.usertitle as UserTitle, g.adminpermissions as AdminPermissions 
+                FROM pm, pmtext AS txt
+                LEFT JOIN user u ON(u.userid = txt.fromuserid)
+                LEFT JOIN usergroup g ON(g.usergroupid = u.usergroupid)
+                WHERE pm.pmtextid = txt.pmtextid
+                AND pm.userid = @userId " +
+                (readState != null ? "AND pm.messageread = @readStateRaw " : "") + @"
+                ORDER BY txt.dateline DESC
+                LIMIT @count";
 
+            int readStateRaw = (readState.HasValue ? (int)readState.Value : 0);
+            var args = new { userId, readStateRaw, textPreviewWords, count };
+            var pms = db.Query(sql, mappingFunc, args);
+            return pms.ToList();
+        }
     }
 }
--- ULabs.VBulletinEntityDemo/Controllers/LightManagerController.cs
+++ ULabs.VBulletinEntityDemo/Controllers/LightManagerController.cs
@@ -5,32 +5,68 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ULabs.VBulletinEntity.Attributes;
 using ULabs.VBulletinEntity.LightManager;
+using ULabs.VBulletinEntity.LightModels.Forum;
 using ULabs.VBulletinEntity.Manager;
+using ULabs.VBulletinEntity.Models.Manager;
 using ULabs.VBulletinEntity.Models.Permission;
 using ULabs.VBulletinEntityDemo.Models;
+using ULabs.VBulletinEntity.Tools;
+using ULabs.VBulletinEntity.LightModels.User;
 
 namespace ULabs.VBulletinEntityDemo.Controllers {
     public class LightManagerController : Controller {
         readonly VBLightSessionManager lightSessionManager;
         readonly VBLightForumManager lightForumManager;
         readonly VBLightThreadManager lightThreadManager;
-        public LightManagerController(VBLightSessionManager lightSessionManager, VBLightForumManager lightForumManager, VBLightThreadManager lightThreadManager) {
+        readonly VBLightSettingsManager settingsManager;
+        readonly VBLightUserManager lightUserManager;
+        public LightManagerController(VBLightSessionManager lightSessionManager, VBLightForumManager lightForumManager, VBLightThreadManager lightThreadManager, VBLightSettingsManager settingsManager,
+            VBLightUserManager lightUserManager) {
             this.lightSessionManager = lightSessionManager;
             this.lightForumManager = lightForumManager;
             this.lightThreadManager = lightThreadManager;
+            this.settingsManager = settingsManager;
+            this.lightUserManager = lightUserManager;
+
+            var commonSettings = settingsManager.CommonSettings;
         }
         public IActionResult Dashboard() {
             var session = lightSessionManager.GetCurrent();
             var model = new LightDashboardModel(lightForumManager, lightThreadManager, lightSessionManager);
-
+            var thx = lightThreadManager.GetThanks(userId: 18, afterTimestamp: 1566595766);
             lightSessionManager.UpdateLastActivity(session.SessionHash, "/LightTest");
+            var threadModel = new LightCreateThreadModel(session.User, forumId: 58, title: "Auto Testthread LightThreadManager", text: "Automatisch erzeugter Testthread", ipAddress: "127.0.0.1");
+            //int tid = lightThreadManager.CreateThread(threadModel);
+            int lastReadTs = lightThreadManager.GetContentReadTime(contentId: 38325, userId: 18);
+            var lastReadDt = lastReadTs.ToDateTime();
+            var dtLocal = lastReadDt.ToLocalTime();
+
+            int unreadReplyId = lightThreadManager.GetNextUnreadReplyId(threadId: 38043, lastReadTime: 1566659655);
+            int page = lightThreadManager.GetPageOfReply(threadId: 38043, replyId: 441387);
+            var newestSmalltalkReplys = lightThreadManager.GetNewestReplys(threadId: 29780);
+
+            var adminTest = lightThreadManager.GetNewestThreads(8, onlyWithoutReplys: true, excludedForumIds: new List<int>(), orderByLastPostDate: false);
+            var pms = lightUserManager.GetPrivateMessages(userId: 18, VBPrivateMessageReadState.Unread, textPreviewWords: 1);
+            var pms2 = lightUserManager.GetPrivateMessages(userId: 18, VBPrivateMessageReadState.Unread);
+            var pms3 = lightUserManager.GetPrivateMessages(userId: 18);
             return View(model);
         }
-        public IActionResult ViewThread(int id) {
-            var model = new ViewThreadModel(lightThreadManager, id, page: 1, lightSessionManager.GetCurrent().User.Id);
+        public IActionResult ViewThread(int id, int page = 1) {
+            var model = new ViewThreadModel(lightThreadManager, id, page, lightSessionManager.GetCurrent().User.Id);
+            var invisibleReplys = lightThreadManager.GetInvisibleReplys(model.Thread.Id, model.ReplysInfo);
             return View(model);
         }
-        [VBLightAuthorize]
+
+        public IActionResult Reply(int id) {
+            var replyModel = new LightCreateReplyModel(lightSessionManager.GetCurrent().User, forumId: 73, threadId: id, text: "Testreply new light manager", ipAddress: "127.0.0.1");
+            var check = lightThreadManager.CreateReplyCheck(replyModel);
+            if(check == CanReplyResult.Ok) {
+                int postId = lightThreadManager.CreateReply(replyModel);
+                return Content("Check Ok");
+            }
+            return Content("Failed: " + check.ToString());
+        }
+        [VBLightAuthorize(permissionRedirectUrl: "/LightManager/Dashboard", requiredUserGroupId: 9)]
         public IActionResult Authorized() {
             return Content("Youre authorized!");
         }
