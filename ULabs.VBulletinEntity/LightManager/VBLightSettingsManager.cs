d3e3b0be957687bf2c31826a3ff8a85fcd79b0b8
DMW007 <danielw@posteo.de>
2019-08-30 20:36:32 +0200

    Added cookie domain to common settings

--- ULabs.VBulletinEntity/LightManager/VBLightSettingsManager.cs
+++ ULabs.VBulletinEntity/LightManager/VBLightSettingsManager.cs
@@ -26,7 +26,7 @@ namespace ULabs.VBulletinEntity.LightManager {
                     string sql = @"
                         SELECT varname, value
                         FROM setting
-                        WHERE varname IN('bburl', 'recycle_forum', 'maxposts')";
+                        WHERE varname IN('bburl', 'recycle_forum', 'maxposts', 'cookiedomain')";
                     var rawSettings = db.Query(sql).ToDictionary(
                         row => (string)row.varname,
                         row => (string)row.value
--- ULabs.VBulletinEntity/LightModels/VBLightCommonSettings.cs
+++ ULabs.VBulletinEntity/LightModels/VBLightCommonSettings.cs
@@ -10,10 +10,12 @@ namespace ULabs.VBulletinEntity.LightModels {
         /// Specified forum id for trash by Addon "Move Threads to Recycle Bin"
         /// </summary>
         public int RecycleBinForumId { get; set; }
+        public string CookieDomain { get; set; }
 
         public VBLightCommonSettings(Dictionary<string,string> sqlData) {
             BaseUrl = sqlData["bburl"];
             PostsPerPage = int.Parse(sqlData["maxposts"]);
+            CookieDomain = sqlData["cookiedomain"];
 
             // Checking for the presence of Addon settings make the class useable for users without having this addon installed
             if(sqlData.ContainsKey("recycle_forum")) {
