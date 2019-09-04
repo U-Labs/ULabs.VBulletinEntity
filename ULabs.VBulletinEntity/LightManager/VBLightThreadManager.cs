9d302777c6a1a4e516c39b03433471ab8fc06034
DMW007 <danielw@posteo.de>
2019-09-04 02:28:22 +0200

    Added counter how much unread replys are present

--- ULabs.VBulletinEntity/LightManager/VBLightThreadManager.cs
+++ ULabs.VBulletinEntity/LightManager/VBLightThreadManager.cs
@@ -254,7 +254,7 @@ namespace ULabs.VBulletinEntity.LightManager {
             // Grouping by contentid (which is the thread id) avoid returning a row for each post the user made in this thread
             // ContentId 2 = Threads
             string sql = @"
-                SELECT r.contentid AS ThreadId, 
+                SELECT COUNT(r.contentid) as UnreadReplysCount, r.contentid AS ThreadId, 
 					t.title AS ThreadTitle, t.lastpost AS LastPostTimeRaw,
 				    f.forumid AS ForumId, f.title AS ForumTitle,
 					u.userid AS LastPosterUserId, u.avatarrevision AS LastPosterAvatarRevision
--- ULabs.VBulletinEntity/LightModels/VBLightUnreadActiveThread.cs
+++ ULabs.VBulletinEntity/LightModels/VBLightUnreadActiveThread.cs
@@ -12,7 +12,7 @@ namespace ULabs.VBulletinEntity.LightModels {
         public int LastPostTimeRaw { get; set; }
         public int LastPosterUserId { get; set; }
         public int LastPosterAvatarRevision { get; set; }
-
+        public int UnreadReplysCount { get; set; }
         public DateTime LastPostTime {
             get => LastPostTimeRaw.ToDateTime();
             set => LastPostTimeRaw = DateTimeExtensions.ToUnixTimestampAsInt(value);
