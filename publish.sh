a2917bbfc60023b83bb6587a4293c597bad6681f
DMW007 <21245043+DMW007@users.noreply.github.com>
2020-05-23 16:22:47 +0200

    Simplified publish.sh, added more debug logs

--- publish.sh
+++ publish.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
+set -x
 BAGET_API_KEY=$1
 BAGET_BASE_URL=$2
-FULL_PROJECT_PATH=$3
-PROJECT_NAME=$4
+PROJECT_PATH=$3
+PROJECT=$4
 
-function publish() {
-    PROJECT_PATH=$1
-    PROJECT=$2
-    version=$(cat $PROJECT_PATH/$PROJECT.csproj | grep "<Version>" | egrep -o "[0-9\.]+")
-    packageFile=$(ls ${PROJECT_PATH}/bin/Debug | grep ${version})
-    echo "Try to publish ${packageFile}"
+version=$(cat $PROJECT_PATH/$PROJECT.csproj | grep "<Version>" | egrep -o "[0-9\.]+")
+packageFile=$(ls ${PROJECT_PATH}/bin/Debug | grep ${version})
+echo "Try to publish ${packageFile}"
+
+if ! curl -s --fail "https://baget.dev.u-labs.de/v3/registration/${PROJECT}/${version}.json" > /dev/null
+then
+    echo "Not published yet: Start publishing!"
+    dotnet nuget push ${PROJECT_PATH}/bin/Debug/${packageFile} -s ${BAGET_BASE_URL}/v3/index.json -k ${BAGET_API_KEY}
+    publishRc=$?
+    echo "Publish rc = ${publishRc}"
     
-    if ! curl -s --fail "https://baget.dev.u-labs.de/v3/registration/${PROJECT}/${version}.json" > /dev/null
-    then
-        echo "Not published yet: Start publishing!"
-        dotnet nuget push ${PROJECT_PATH}/bin/Debug/${packageFile} -s ${BAGET_BASE_URL}/v3/index.json -k ${BAGET_API_KEY}
-        publishRc=$?
-        echo "Publish rc = ${publishRc}"
-        
-        if [ $publishRc -ne 0 ]; then
-            echo "Nuget publish error: exit with $publishRc"
-            exit $publishRc
-        fi
-    else
-        echo "WARNING: Version ${version} was already published"
+    if [ $publishRc -ne 0 ]; then
+        echo "Nuget publish error: exit with $publishRc"
+        exit $publishRc
     fi
-}
-
-publish $FULL_PROJECT_PATH $PROJECT_NAME
-#publish "/app/ULabs.VBulletinEntity.Shared" "ULabs.VBulletinEntity.Shared"
-#publish "/app/ULabs.LightVBulletinEntity" "ULabs.LightVBulletinEntity"
-#publish "/app/ULabs.VBulletinEntity" "ULabs.VBulletinEntity"
+else
+    echo "WARNING: Version ${version} was already published"
+fi
