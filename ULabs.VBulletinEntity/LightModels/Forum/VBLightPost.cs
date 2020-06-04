31ff1799b32aac26c8d57097105cdecaa0c105a8
DMW007 <21245043+DMW007@users.noreply.github.com>
2020-06-04 20:14:16 +0200

    Add method to generate post text excerpt

--- ULabs.VBulletinEntity/LightModels/Forum/VBLightPost.cs
+++ ULabs.VBulletinEntity/LightModels/Forum/VBLightPost.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using ULabs.VBulletinEntity.LightModels.User;
 using ULabs.VBulletinEntity.Models.Forum;
 using ULabs.VBulletinEntity.Tools;
@@ -26,6 +28,12 @@ namespace ULabs.VBulletinEntity.LightModels.Forum {
         public DateTime CreatedTime {
             get => CreatedTimeRaw.ToDateTime();
             set => CreatedTimeRaw = DateTimeExtensions.ToUnixTimestampAsInt(value);
+        }
+
+        public string GetTextExcerpt(int wordsLimit = 15, string suffix = " ...") {
+            string cleanText = Regex.Replace(Text, @"\[[^\]]+\]", "");
+            string excerpt = string.Join(" ", cleanText.Split().Take(wordsLimit)) + suffix;
+            return excerpt;
         }
     }
 }
